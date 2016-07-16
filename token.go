51f17d48f8e5e5cf710764bcdced78bf6f1c7a92
Jörg Reinhardt <j.p.reinhardt@gmail.com>
2016-07-16 08:53:37 +0200

    integrated the god library and it compiles, so far so good. To make any valuable use of it, further planning is required. The intension of reimplementing a type system is to make it dynamic and to replace the nescessarity for reflection by type assertion, which performs much better, but also reduces complexity.
    To really achieve that, further thought has to be put into it. The
    public interface should in no way shape or form under no circumstances
    ever expose an empty interface, have no need at all for assertion, or
    convertion of a value returned by, or passed to it.
    
    optional intensions are: keeping the error type exclusive to communicate
    errors and not misuse it for type-/, boundary-/, or membership of a set
    checking.  Use if/ok only publicly when aboulutely nescessary and where
    it makes sense, which would be loop iteration and maybe as a tradeof, to
    superunify an interface for types that need significantly different
    methods, which might as well turn out to be a questionable approach in
    the first place.
    
     zum Commit vorgemerkte Änderungen:
    	gelöscht:       agiledoc.test
    	geändert:       token.go
    	neue Datei:     token_test.go
    	geändert:       types.go
    
     Änderungen, die nicht zum Commit vorgemerkt sind:
    	geändert:       vendor/github.com/emirpasic (geänderter Inhalt, unversionierter Inhalt)

--- token.go
+++ token.go
@@ -9,27 +9,21 @@ package agiledoc
 
 import (
 	"bytes"
-	i "github.com/emirpasic/gods/containers"
-	l "github.com/emirpasic/gods/lists/doublylinkedlist"
-	m "github.com/emirpasic/gods/maps/hashmap"
-	"sync"
 )
 
 // the position type provides integer indices that reference the byte indecx of
 // the start and end point of a token relative to its containing context.
 // input
-type pos [2]int
-
-// the ident type is the string that expresses the name of a ident. idents can
-// be expressed as bit flags, or be string keys in a parameter
-type ident string
+type pos [2]uint
 
 // a parameter has an identifyer either in the form of a string, or uint
 // byteflag and carrys a value.
 type parm struct {
-	*ident
-	val Val
+	Val
+	id string
 }
+
+func (p parm) Key() string { return string(p.id) }
 
 // The token type combines a type with a position marker and a list of
 // parameters. The parameter list is implemented using the gods library to
@@ -37,96 +31,112 @@ type parm struct {
 // implemented bu gods hashmap. While encapsulating the empty interface the god
 // interface has to expose being universal.
 type token struct {
-	ttype    TType
-	position pos
-	params   i.Container // contains god hashmap
+	ttype     TType
+	position  pos
+	Container // contains god hashmap
 }
-type TType uint
+type TType uint32
 
-func newToken() token {
-	return token{
-		0,
-		pos{},
-		m.New(), // implements god Container
-	}
-}
-
-//// semaQueue
-///
-// The parsers semaQueue will be implemented by gods doubly linked list. that keeps
-// the whole content serialized, manipulateable and accessable. Being universal
-// god has to expose the empty interface, the implementation will use the more
-// specific Val interface instead, encapsulating the empty interface by
-// assertion.
-//
-// A mutex keeps the implementation thread safe.
-type semaQueue struct {
-	*sync.RWMutex
-	i.Container
-}
-
-func newsemaQueue() *semaQueue {
-	return &semaQueue{
-		&sync.RWMutex{},
-		l.New(),
-	}
-}
-
-//// semaBuf
-///
-// a lockable semaBuf that contains the input stream.
-type semaBuf struct {
-	*sync.RWMutex
-	*bytes.Buffer
-}
-
-func newsemaBuf() *semaBuf {
-	return &semaBuf{
-		&sync.RWMutex{},
-		bytes.NewBuffer([]byte{}),
-	}
-}
-
-//// PARSER
-///
-type parser struct {
-	*semaBuf   // contains document as bute slice
-	*semaQueue // contains tokens to be parsed
-}
+//go:generate -command stringer -type TType ./token.go
+const (
+	AutoLink TType = 1 << iota
+	BlockCode
+	BlockHtml
+	BlockQuote
+	CodeSpan
+	DocumentFooter
+	DocumentHeader
+	DoubleEmphasis
+	Emphasis
+	Entity
+	FootnoteItem
+	FootnoteRef
+	Footnotes
+	GetFlags
+	HRule
+	Header
+	Image
+	LineBreak
+	Link
+	List
+	ListItem
+	NormalText
+	Paragraph
+	RawHtmlTag
+	StrikeThrough
+	Table
+	TableCell
+	TableHeaderCell
+	TableRow
+	TitleBlock
+	TripleEmphasis
+)
 
 //// TOKENIZER
-///
+/// TODO: evaluate if queue/semafore performs any better
 // tokenizer implements blackfriday renderer. token content is written to
-// semaBuf, token instance is appended to semaQueue
+// semaBuf, token instance is appended to semaQueue of the contained parser
 type tokenizer struct {
-	flags      int
-	curPos     int
-	*semaBuf   // references parsers semaBuf
-	*semaQueue // references parsers semaQueue
+	// options blackfriday.Options // parameters
+	flags   flagVal    // options
+	current uint       // current position
+	out     chan token // returns tokens to the caller
 }
 
-// Header and footer
+func (tkz *tokenizer) newToken(t TType, raw []byte, parms ...parm) {
+	var o Container
+	// copy parameters
+	if len(parms) > 0 {
+		// allocate array to take parameters
+		o = newContainer(array).(*cont)
+
+		for _, parm := range parms {
+			o.(*cont).Add(parm)
+		}
+	}
+	// calculate new position
+	pos := pos{(*tkz).current, (*tkz).current + uint(len(raw))}
+
+	// generate token of designated type, with calculated position and the options container
+	tok := token{t, pos, o}
+
+	// put token into channel
+	(*tkz).out <- tok
+
+	// set new position only AFTER token is emitted
+	(*tkz).current = pos[1] + 1
+}
+
+func NewTokenizer(flags ...uint) (*tokenizer, chan token) {
+	f := NewTypedVal(FLAG, 0).(flagVal)
+	// XOR flags
+	for _, flag := range flags {
+		cmp := NewTypedVal(FLAG, flag).(flagVal)
+		f.Xor(f.Flag(), cmp.Flag())
+	}
+	c := make(chan token, 1)
+	return &tokenizer{f, 0, c}, c
+}
+
+//// BLACK FRIDAY INTERFACE IMPLEMENTATION
+///
+// implement the black friday renderer to act as receiver for the parsed parts
+// of a markdown document. Generates tokens of the token type, that take all
+// provided metadata, parsed and raw data.
+//
+// DOCUMENT METAINFO HEADER AND FOOTER
 func (t *tokenizer) DocumentHeader(out *bytes.Buffer) {
-	// (*t).newtoken(D_HEADER, out.Bytes(), keyVal{"", emptyVal{}})
+	raw := out.Bytes()
+	(*t).newToken(DocumentHeader, raw)
 }
 
 func (t *tokenizer) DocumentFooter(out *bytes.Buffer) {
-	// (*t).newtoken(D_FOOTER, out.Bytes(), keyVal{"", emptyVal{}})
+	raw := out.Bytes()
+	(*t).newToken(DocumentFooter, raw)
 }
 
-// Document Blocks
-func (t *tokenizer) Header(out *bytes.Buffer, text func() bool, level int, id string) { // header as in headline as in section
-	// 	var params []keyVal = []keyVal{}
-	// 	params = append(params, keyVal{"level", newVal(INTEGER, level)})
-	// 	params = append(params, keyVal{"id", newVal(STRING, id)})
-	//
-	// 	if text() { // call callback to generate byteslice and writes it to the semaBuf
-	// 		(*t).newtoken( // generates a new token and sends it to the callers semaQueue
-	// 			SECTION,
-	// 			out.Bytes(),
-	// 			params...,
-	// 		)
-	// 	}
+// DOCUMENT BLOCKS
+func (t *tokenizer) Header(out *bytes.Buffer, text func() bool, level int, id string) { // header as in headline of a section
 }
 func (t *tokenizer) BlockCode(out *bytes.Buffer, text []byte, lang string)                 {}
 func (t *tokenizer) BlockQuote(out *bytes.Buffer, text []byte)                             {}
@@ -160,20 +170,4 @@ func (t *tokenizer) FootnoteRef(out *bytes.Buffer, ref []byte, id int)          
 func (t *tokenizer) Entity(out *bytes.Buffer, entity []byte)   {}
 func (t *tokenizer) NormalText(out *bytes.Buffer, text []byte) {}
 
-func (t *tokenizer) GetFlags() int { return (*t).flags }
-
-func newtokenizer(semaQueue chan token, name string, flags ...int) *tokenizer {
-
-	var nf int = 0
-
-	for _, f := range flags {
-		f := f
-		nf = nf | f
-	}
-
-	return &tokenizer{
-		flags:   nf,
-		curPos:  0,
-		semaBuf: newsemaBuf(),
-	}
-}
+func (t *tokenizer) GetFlags() int { return int((*t).flags.Int64()) }
--- types.go
+++ types.go
@@ -1,9 +1,3 @@
-package agiledoc
-
-import (
-	"math/big"
-)
-
 // TYPE SYSTEM
 //
 // after lots of experimenting, I decided the best way is to keep the interface
@@ -19,10 +13,145 @@ import (
 // its contained native type, but those methods differ by signature and can't
 // be defined without the use of an empty interface. Everything else is up for
 // grabs,
+package agiledoc
+
+import (
+	i "github.com/emirpasic/gods/containers"
+	l "github.com/emirpasic/gods/lists"
+	ar "github.com/emirpasic/gods/lists/arraylist"
+	do "github.com/emirpasic/gods/lists/doublylinkedlist"
+	si "github.com/emirpasic/gods/lists/singlylinkedlist"
+	m "github.com/emirpasic/gods/maps"
+	hbm "github.com/emirpasic/gods/maps/hashbidimap"
+	hm "github.com/emirpasic/gods/maps/hashmap"
+	// tbm "github.com/emirpasic/gods/maps/treebidimap"
+	// tm "github.com/emirpasic/gods/maps/treemap"
+	//s "github.com/emirpasic/gods/sets"
+	sh "github.com/emirpasic/gods/sets/hashset"
+	//st "github.com/emirpasic/gods/sets/treeset"
+	//s "github.com/emirpasic/gods/stacks"
+	sa "github.com/emirpasic/gods/stacks/arraystack"
+	sl "github.com/emirpasic/gods/stacks/linkedliststack"
+	//t "github.com/emirpasic/gods/trees"
+	//bh "github.com/emirpasic/gods/trees/binaryheap"
+	//rbt "github.com/emirpasic/gods/trees/redblacktree"
+	"math/big"
+)
+
+// VALUE INTERFACE
 type Val interface {
 	Type() ValType
 	Value() Val
 }
+
+// CONTAINER INTERFACE
+// interface to conceal god container values interface nature behind the val interface
+type Container interface {
+	// i.cont
+	Empty() bool
+	Size() int
+	Clear()
+	Values() []Val
+}
+
+// CONTAINER IMPLEMENTATION
+// wraps the container in a struct
+type cont struct {
+	contType
+	i.Container
+}
+
+// replace god containers value method through a method that encapsulates the
+// empty value.
+func (c cont) Values() (r []Val) {
+	r = []Val{}
+	for _, v := range c.Values() {
+		v := v.(Val)
+		r = append(r, v)
+	}
+	return r
+}
+func (c *cont) Add(v ...Val) {
+	t := (*c).contType
+	switch {
+	case t&lists != 0:
+		c.Container.(l.List).Add(v)
+	case t&maps != 0:
+		for _, val := range v {
+			v := val.(parm)
+			(*c).Container.(m.Map).Put(v.Key(), v.Value())
+		}
+	case t&stacks != 0:
+		// c.Container.(sa.Stack).Push(v)
+	case t&sets != 0:
+		//(c.Container).(st.Set).Add(v)
+		//	case t&trees != 0:
+		//		if t&binheap != 0 {
+		//			(*c).Container.(t.Heap).Push(v)
+		//		} else {
+		//			(*c).Container.(t.Tree).Put(v)
+		//		}
+	}
+}
+
+// return an empty container of a given type
+func newContainer(t contType) Container {
+	switch t {
+	case array:
+		return &cont{t, ar.New()}
+	case double:
+		return &cont{t, do.New()}
+	case single:
+		return &cont{t, si.New()}
+	case hashbidi:
+		return &cont{t, hbm.New()}
+	case hash:
+		return &cont{t, hm.New()}
+	case treebidi:
+		// return &cont{t,tbm.New()}
+	case treemap:
+		// return &cont{t,tm.New()}
+	case hashset:
+		return &cont{t, sh.New()}
+	case treeset:
+		// return &cont{t,st.New()}
+	case arraystack:
+		return &cont{t, sa.New()}
+	case linkedstack:
+		return &cont{t, sl.New()}
+	case binheap:
+		// return &cont{t,bh.New()}
+	case redblack:
+		// return &cont{t,rbt.New()}
+	}
+	return &cont{}
+}
+
+// container type marks the type of container taken from the god library
+type contType uint16
+
+const (
+	array contType = 1 << iota
+	single
+	double
+	hash
+	hashbidi
+	tree
+	treebidi
+	treemap
+	hashset
+	treeset
+	arraystack
+	linkedstack
+	binheap
+	redblack
+
+	lists  = array | single | double
+	maps   = hash | hashbidi | tree | treebidi
+	sets   = hashset | treeset
+	stacks = arraystack | linkedstack
+	trees  = binheap | redblack
+)
 
 // the base types are kept as simple as possible.
 //
@@ -100,12 +229,12 @@ func (v strVal) Value() Val   { return v }
 // tyoed return functions return values of generic type, each is implemented by
 // at least the types containing that native type, and all that can be
 // converted to it.
-func (emptyVal) Empty() emptyVal { return emptyVal{} }
-func (v flagVal) Flag() uint     { return uint(v.Int64()) }
-func (v intVal) Integer() int    { return int(v.Int64()) }
-func (v floatVal) Flt() float64  { f, _ := v.Float64(); return float64(f) }
-func (v byteVal) Byte() []byte   { return v }
-func (v strVal) String() string  { return string(v) }
+func (emptyVal) Empty() emptyVal   { return emptyVal{} }
+func (v flagVal) Flag() *big.Int   { return v.Int }
+func (v intVal) Integer() *big.Int { return v.Int }
+func (v floatVal) Flt() *big.Float { return v.Float }
+func (v byteVal) Byte() []byte     { return v }
+func (v strVal) String() string    { return string(v) }
 
 // if the native tyoe is allready known at the time of initialization,
 // reflection can be omitted.

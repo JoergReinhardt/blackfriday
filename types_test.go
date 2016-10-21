8fdcf167cbb8455008031b0b855bcfc5ce6b71a3
Jörg Reinhardt <j.p.reinhardt@gmail.com>
2016-10-21 22:04:00 +0200

    Wrote tests to test for all possible permutations of boolean algebra calculateable with public Bool's method set.

--- types_test.go
+++ types_test.go
@@ -1,28 +1,85 @@
 package agiledoc
 
 import (
+	"fmt"
 	"testing"
 )
 
-const (
-	TRUE  bool = true
-	FALSE bool = false
-)
-
-var v Evaluable
-
-func TestBoolInstanceiation(t *testing.T) {
-	v = Value(TRUE)
-	(*t).Log(v)
-
+var tests = []struct {
+	x   bool
+	y   bool
+	exp string
+	op  string
+}{
+	{true, true, "true", "and"},
+	{false, false, "false", "and"},
+	{true, false, "false", "and"},
+	{false, true, "false", "and"},
+	{true, true, "true", "andnot"},
+	{false, false, "false", "andnot"},
+	{true, false, "false", "andnot"},
+	{false, true, "false", "andnot"},
+	{true, true, "false", "not"},
+	{false, false, "false", "not"},
+	{true, false, "false", "not"},
+	{false, true, "false", "not"},
+	{false, true, "true", "or"},
+	{true, true, "true", "or"},
+	{true, false, "true", "or"},
+	{false, false, "false", "or"},
+	{false, true, "true", "xor"},
+	{true, true, "false", "xor"},
+	{true, false, "true", "xor"},
+	{false, false, "false", "xor"},
 }
-func TestBoolAnd(t *testing.T) {
-	(*t).Log(v.Type())
-	(*t).Log(v.(Bool).And(v.(Bool), Value(FALSE).(Bool)))
-	(*t).Log(v.(Bool).And(v.(Bool), Value(TRUE).(Bool)))
-}
-func TestBoolAndNot(t *testing.T) {
-	(*t).Log(v.Type())
-	(*t).Log(v.(Bool).AndNot(v.(Bool), Value(FALSE).(Bool)))
-	(*t).Log(v.(Bool).AndNot(v.(Bool), Value(TRUE).(Bool)))
+
+func TestBool(t *testing.T) {
+	for _, te := range tests {
+		x := Value(te.x).(Bool)
+		y := Value(te.y).(Bool)
+		exp := te.exp
+		switch te.op {
+		case "and":
+			if x.And(x, y).String() != exp {
+				(*t).Fail()
+				(*t).Log("failed op: " + te.op +
+					" x: " + fmt.Sprint(te.x) +
+					" y: " + fmt.Sprint(te.y) +
+					" expected: " + te.exp)
+			}
+		case "andnot":
+			if x.And(x, y).String() != exp {
+				(*t).Fail()
+				(*t).Log("failed op: " + te.op +
+					" x: " + fmt.Sprint(te.x) +
+					" y: " + fmt.Sprint(te.y) +
+					" expected: " + te.exp)
+			}
+		case "or":
+			if x.Or(x, y).String() != exp {
+				(*t).Fail()
+				(*t).Log("failed op: " + te.op +
+					" x: " + fmt.Sprint(te.x) +
+					" y: " + fmt.Sprint(te.y) +
+					" expected: " + te.exp)
+			}
+		case "xor":
+			if x.Xor(x, y).String() != exp {
+				(*t).Fail()
+				(*t).Log("failed op: " + te.op +
+					" x: " + fmt.Sprint(te.x) +
+					" y: " + fmt.Sprint(te.y) +
+					" expected: " + te.exp)
+			}
+		case "not":
+			if x.Not(x).String() != exp {
+				(*t).Fail()
+				(*t).Log("failed op: " + te.op +
+					" x: " + fmt.Sprint(te.x) +
+					" y: " + fmt.Sprint(te.y) +
+					" expected: " + te.exp)
+			}
+		}
+		(*t).Log(te)
+	}
 }

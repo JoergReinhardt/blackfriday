d4f78dcf9c22e524b9727046784b45f43bfea763
Jörg Reinhardt <j@sabayon.local>
2016-11-01 21:54:05 +0100

    more methods accompanying the ratio type

--- types/ratio.go
+++ types/ratio.go
@@ -9,9 +9,6 @@ func (r Ratio) Serialize() []byte { return []byte(ratio(r).String()) }
 func (r Ratio) String() string    { return r().String() }
 func (r Ratio) Type() ValueType   { return RATIONAL }
 
-func (r Ratio) Integer() Integer { return ratPool.Get().(Ratio).SetValue(r).Integer() }
-func (r Ratio) Float() Float     { return wrap(r()).(Float) }
-
 func (r Ratio) SetKey(v Evaluable) Ratio   { return wrap(pairPool.Get().(ratio).Ratio()).(Ratio) }
 func (r Ratio) SetValue(v Evaluable) Ratio { return wrap(pairPool.Get().(ratio).Ratio()).(Ratio) }
 func (r Ratio) SetNum(v Evaluable) Ratio   { return r.SetValue(v) }
@@ -24,4 +21,22 @@ func (r Ratio) SetFrac(a, b Evaluable) Ratio {
 }
 func (r Ratio) Abs(v Ratio) Ratio {
 	return wrap(ratPool.Get().(ratio).abs(v())).(Ratio)
+}
+func (r Ratio) Add(v Ratio) Ratio {
+	return wrap(ratio(r).add(r(), v())).(Ratio)
+}
+func (r Ratio) Cmp(v Ratio) Ratio {
+	return wrap(ratio(r).cmp(v())).(Ratio)
+}
+func (r Ratio) Neg(v Ratio) Ratio {
+	return wrap(ratio(r).neg(v())).(Ratio)
+}
+func (r Ratio) Mul(v Ratio) Ratio {
+	return wrap(ratio(r).mul(r(), v())).(Ratio)
+}
+func (r Ratio) Quo(v Ratio) Ratio {
+	return wrap(ratio(r).quo(r(), v())).(Ratio)
+}
+func (r Ratio) Sub(v Ratio) Ratio {
+	return wrap(ratio(r).sub(r(), v())).(Ratio)
 }
